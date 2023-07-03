25ee5e8528aa78c0e07678984e37e1cbb5f807b4
fuadAbdiaziz <fuadabdaz99@gmail.com>
2023-07-03 10:35:40 -0700

    done

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+# Create heroes
+hero1 = Hero.create(name: "Kamala Khan", super_name: "Ms. Marvel")
+hero2 = Hero.create(name: "Doreen Green", super_name: "Squirrel Girl")
+hero3 = Hero.create(name: "Gwen Stacy", super_name: "Spider-Gwen")
+
+# Create powers
+power1 = Power.create(name: "super strength", description: "gives the wielder super-human strengths")
+power2 = Power.create(name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed")
+power3 = Power.create(name: "super speed", description: "allows the wielder to move at incredible speeds")
+
+# Create hero powers
+HeroPower.create(hero: hero1, power: power1, strength: "Strong")
+HeroPower.create(hero: hero1, power: power2, strength: "Average")
+HeroPower.create(hero: hero2, power: power1, strength: "Average")
+HeroPower.create(hero: hero2, power: power3, strength: "Weak")
+HeroPower.create(hero: hero3, power: power1, strength: "Average")
+HeroPower.create(hero: hero3, power: power2, strength: "Weak")
